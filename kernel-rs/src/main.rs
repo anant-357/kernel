61e04d963a6249e4dc2497141c545f9ef5125d44
anant-357 <anantsh.88@gmail.com>
2023-12-17 20:53:58 +0530

    vga support

--- kernel-rs/src/main.rs
+++ kernel-rs/src/main.rs
@@ -8,7 +8,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static START: &[u8] = b"The Beginning!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga = 0xb8000 as *mut u8;
+
+    for (i, &byte) in START.iter().enumerate() {
+        unsafe {
+            *vga.offset(i as isize * 2) = byte;
+            *vga.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
