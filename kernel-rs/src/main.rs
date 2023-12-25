02c04fad3f175a91f06059e6538abb0a5907752a
anant-357 <anantsh.88@gmail.com>
2023-12-25 12:57:24 +0530

    uefi and legacy bios support

--- kernel-rs/src/main.rs
+++ kernel-rs/src/main.rs
@@ -1,20 +1,20 @@
-#![no_std]
-#![no_main]
+fn main() {
+    // read env variables that were set in build script
+    let uefi_path = env!("UEFI_PATH");
+    let bios_path = env!("BIOS_PATH");
 
-mod vga;
-use bootloader_api::{entry_point, BootInfo};
-use core::panic::PanicInfo;
+    // choose whether to start the UEFI or BIOS image
+    let uefi = true;
 
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+    let mut cmd = std::process::Command::new("qemu-system-x86_64");
+    if uefi {
+        cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
+        cmd.arg("-drive")
+            .arg(format!("format=raw,file={uefi_path}"));
+    } else {
+        cmd.arg("-drive")
+            .arg(format!("format=raw,file={bios_path}"));
+    }
+    let mut child = cmd.spawn().unwrap();
+    child.wait().unwrap();
 }
-
-#[no_mangle]
-fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
-    println!(" The Beginning {:#?}", boot_info);
-    loop {}
-}
-
-entry_point!(kernel_main);
